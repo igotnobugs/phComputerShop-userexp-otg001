472939c536c62ee7a6232a73e1cf3b0329fa478e
seanfargas <seandavid.fargas@benilde.edu.ph>
2020-10-23 14:26:49 +0800

    Basic UI - In progress

---
+++ Assets/Scrips/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int cost;
+    public string itemName;
+
+    public void bought()
+    {
+        if(GetComponentInParent<Shop>().currency >= cost)
+        {
+            GetComponentInParent<Shop>().currency -= cost;
+            GetComponentInParent<Shop>().addItem(itemName);
+        }
+    }
+}
