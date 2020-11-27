d538e1ac2a8da0133c0306d67aa21f6b35fe4887
Alessandro P. Aguilar <alessandrodiegosebastian.aguilar@benilde.edu.ph>
2020-11-27 20:23:36 +0800

    Merge branch 'main' of https://github.com/igotnobugs/phComputerShop-userexp-otg001 into Dialouge_System
    # Conflicts:
    #	Assets/Scripts/Dialogue/DialogueManager.cs

---
+++ Assets/Scripts/Dialogue/DialogueBox.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+/* Modified to be scaleable and extendable
+ */
+
+[RequireComponent(typeof(EventTrigger))]
+public class DialogueBox : MonoBehaviour, IPointerClickHandler  
+{
+    [SerializeField] private TextMeshProUGUI nameText = null;
+    [SerializeField] TextMeshProUGUI sentenceText = null;
+    public Button nextButton;
+    public TransistionUI panelTransistion;
+
+    private string sentenceToType;
+    private IEnumerator typingCoroutine;
+    private bool isTypingDone = false;
+    private Action nextAction;
+
+    private void Awake() {
+        if (nameText == null || sentenceText == null) {
+            Debug.Log("Assign the text fields in Dialogue Panel");
+        }
+        else {
+            nameText.text = "";
+            sentenceText.text = "";
+        }
+    }
+
+    private void Start() {
+        typingCoroutine = TypeSentence();
+        nextButton.gameObject.SetActive(false);
+    }
+
+    public void NextButtonOnClick(Action onClick) {
+        nextAction = onClick;
+        nextButton.onClick.AddListener(() => nextAction?.Invoke());
+    }
+
+    public void Set(string name, string sentence) {
+        nameText.text = name;
+        sentenceToType = sentence;
+    }
+
+    public void AdvanceDialogue() {
+        // Stop last typing routine
+        StopCoroutine(typingCoroutine);
+
+        // Start a new one
+        typingCoroutine = TypeSentence();
+        StartCoroutine(typingCoroutine);
+    }
+
+    private IEnumerator TypeSentence() {
+        sentenceText.text = "";
+        isTypingDone = false;
+        nextButton.gameObject.SetActive(false);
+        foreach (char letter in sentenceToType.ToCharArray()) {
+            sentenceText.text += letter;
+            yield return null;
+        }
+        isTypingDone = true;
+        nextButton.gameObject.SetActive(true);
+        yield break;
+    }
+
+    public virtual void OnPointerClick(PointerEventData eventData) {
+        if (!isTypingDone) {
+            StopCoroutine(typingCoroutine);
+            isTypingDone = true;
+
+            sentenceText.text = sentenceToType;
+            nextButton.gameObject.SetActive(true);
+        }
+        else {
+            nextAction?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Interactive PopUp/PopUp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+/* Attached to a pop-up prefab
+ * Created by a popup manager
+ * 
+ */
+
+public class PopUp : MonoBehaviour 
+{
+    [SerializeField] private TextMeshProUGUI popUpTitle = null;
+    [SerializeField] private TextMeshProUGUI popUpText = null;
+
+    [SerializeField] private Button closeButton = null;
+
+    private Canvas canvas;
+    private RectTransform rectTransform;
+    private RectTransform canvasRect;
+    private BaseUI targetUI;
+    private Action onCompleteFunc; 
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        canvasRect = canvas.GetComponent<RectTransform>();
+    }
+
+    private void Start() {
+        closeButton.onClick.AddListener(() => {
+            DestroyPopup();
+        });
+    }
+
+    // When initiated, must run the Init
+    public void Init(string title, string content, BaseUI ui = null) {
+        popUpTitle.text = title;
+        popUpText.text = content;
+        targetUI = ui;
+    }
+
+    public void MoveTo(Vector2 worldPos) {
+        rectTransform.position = worldPos;
+    }
+
+    public void SetListener(BaseUI targetUI) {
+        targetUI.OnActivating += DestroyPopup;
+    }
+
+    public void SetOnComplete(Action function) {
+        onCompleteFunc = function;
+    }
+
+    public void DestroyPopup() {
+        onCompleteFunc?.Invoke();
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy() {
+        targetUI.OnActivating -= DestroyPopup;
+    }
+}
